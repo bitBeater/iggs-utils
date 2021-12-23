558250c30dde36d5c7104aa2e87d15279a2964d0
alexrr2iggs <alexandru.bunduche@yahoo.com>
2021-12-23 18:10:47 +0100

    1.1.19

---
+++ src/object.ts
@@ -0,0 +1,36 @@
+
+export interface PathKeysOptions{
+    ignoreFunctions?:boolean,
+    unfoldArrays?:boolean,
+    omitFirstLevel:boolean,
+}
+
+export function getPathKeys(obj: any, opt?:PathKeysOptions): string[] {
+	if (!obj) return;
+	if(typeof obj === 'string') return;
+
+	const pathKeys = [];
+	for (const rawKey of Object.keys(obj)) {
+
+		const value = obj[rawKey];
+
+		if (typeof value === "function" && opt?.ignoreFunctions) continue;
+
+		const key=Array.isArray(obj)?'['+rawKey+']':rawKey;
+
+        if(Array.isArray(value) || (typeof value !== 'object' || !opt?.omitFirstLevel)) pathKeys.push(key);
+		
+		if (typeof value === 'object') {
+			if (Array.isArray(value) && !opt?.unfoldArrays) continue;
+
+			const secondLevelSeparator = Array.isArray(value)?'':'.'
+				
+			const secondLevelKeys = getPathKeys(value,opt);
+			if (!secondLevelKeys?.length) continue;
+			const secondLevelPathKeys = secondLevelKeys.map((k) => key + secondLevelSeparator + k);
+			pathKeys.push(...secondLevelPathKeys);
+		}
+	}
+
+	return pathKeys;
+}
---
+++ src/revivers.ts
@@ -0,0 +1,35 @@
+
+import { ISO_8601_DATE_FORMAT_REX } from './consts';
+
+/**
+ * @link  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse#using_the_reviver_parameter
+ */
+export type Reviver<T>=(key: string, value: any)=>T
+
+/**
+ * revive 
+ * @param _key 
+ * @param value 
+ * @returns 
+ */
+export const ISO_8601StringtoDate:Reviver<Date> = (_key: string, value: any) => (typeof value === 'string' && ISO_8601_DATE_FORMAT_REX.test(value) ? new Date(value) : value);
+
+
+/**
+ * merge multiple reviver in one, the resulted value is equal to the result of the first reviver that resolve,
+ * so reviver order determine theire priority
+ * 
+ * @param revivers 
+ * @returns 
+ * @link  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse#using_the_reviver_parameter
+ */
+export function mergeRevivers(...revivers:Reviver<any>[] ):Reviver<any>{
+    if(!revivers?.length) throw new Error("can't merge revivers: revivers are required");
+    return (key: string, value: any)=>{
+        for(const reviver of revivers){
+            const revived = reviver(key,value);
+            if(revived!==value) return revived;
+        }
+        return value;
+    }
+}
