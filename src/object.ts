b6f963b7aa73a726c7be2ecaf57b21cd0a70be16
alexrr2iggs <alexandru.bunduche@yahoo.com>
2021-12-27 12:38:08 +0100

    f

--- src/object.ts
+++ src/object.ts
@@ -36,7 +36,7 @@ export interface PathKeysOptions {
  * //	 'nestedObject.arr' ]
  * ```
  *
- *  * @example <caption>Omitting first level</caption>
+ *@example <caption>Omitting first level</caption>
  * ```js
  * const object={
  * 	str:"hello",
