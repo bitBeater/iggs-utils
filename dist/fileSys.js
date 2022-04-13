a3615f5eae6b3008501986bead95dd474e553461
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-04-13 19:14:52 +0200

    add write file

--- dist/fileSys.js
+++ dist/fileSys.js
@@ -1,31 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.appendFile = exports.exists = exports.deserealizeObject = exports.serealizeObject = exports.readGZip = exports.writeGZip = exports.fileLines = exports.insertBetweenPlacweHolders = exports.readJson = exports.writeJson = exports.write = exports.writeToDesktop = exports.writeObjectToDesktop = exports.DESKTOP_PATH = void 0;
+exports.of = exports.write = exports.appendFile = exports.exists = exports.deserealizeObject = exports.serealizeObjectSync = exports.readGZip = exports.writeGZipSync = exports.fileLinesSync = exports.insertBetweenPlacweHoldersSync = exports.readJson = exports.writeJsonSync = exports.writeSync = exports.writeToDesktopSync = exports.writeObjectToDesktopSync = exports.DESKTOP_PATH = void 0;
 const fs_1 = require("fs");
 const promises_1 = require("fs/promises");
 const os_1 = require("os");
 const path_1 = require("path");
 const zlib_1 = require("zlib");
 exports.DESKTOP_PATH = (0, path_1.join)((0, os_1.homedir)(), 'Desktop');
-function writeObjectToDesktop(fileName, object) {
+function writeObjectToDesktopSync(fileName, object) {
     (0, fs_1.writeFileSync)(`${exports.DESKTOP_PATH}/${fileName}`, JSON.stringify(object));
 }
-exports.writeObjectToDesktop = writeObjectToDesktop;
-function writeToDesktop(fileName, data) {
+exports.writeObjectToDesktopSync = writeObjectToDesktopSync;
+function writeToDesktopSync(fileName, data) {
     (0, fs_1.writeFileSync)(`${exports.DESKTOP_PATH}/${fileName}`, data);
 }
-exports.writeToDesktop = writeToDesktop;
-function write(dir, fileName, data) {
+exports.writeToDesktopSync = writeToDesktopSync;
+function writeSync(dir, fileName, data) {
     if (!(0, fs_1.existsSync)(dir)) {
         (0, fs_1.mkdirSync)(dir);
     }
     (0, fs_1.writeFileSync)(`${dir}/${fileName}`, data);
 }
-exports.write = write;
-function writeJson(path, object) {
+exports.writeSync = writeSync;
+function writeJsonSync(path, object) {
     (0, fs_1.writeFileSync)(path, JSON.stringify(object));
 }
-exports.writeJson = writeJson;
+exports.writeJsonSync = writeJsonSync;
 function readJson(path, reviver) {
     const data = (0, fs_1.readFileSync)(path);
     if (!data)
@@ -34,7 +34,7 @@ function readJson(path, reviver) {
     return retVal;
 }
 exports.readJson = readJson;
-function insertBetweenPlacweHolders(filePath, data, beginPlaceHolder, endPlaceHolder) {
+function insertBetweenPlacweHoldersSync(filePath, data, beginPlaceHolder, endPlaceHolder) {
     var _a, _b, _c, _d, _e, _f;
     const writeData = (0, fs_1.readFileSync)(filePath);
     if (!(0, fs_1.existsSync)(filePath)) {
@@ -45,8 +45,8 @@ function insertBetweenPlacweHolders(filePath, data, beginPlaceHolder, endPlaceHo
     const bottom = (_f = (_e = (_c = fileContent === null || fileContent === void 0 ? void 0 : fileContent.split) === null || _c === void 0 ? void 0 : (_d = _c.call(fileContent, endPlaceHolder)).reverse) === null || _e === void 0 ? void 0 : _e.call(_d)) === null || _f === void 0 ? void 0 : _f[0];
     (0, fs_1.writeFileSync)(filePath, `${top}\n\r${beginPlaceHolder}\n\r${data}\n\r${endPlaceHolder}\n\r${bottom}`);
 }
-exports.insertBetweenPlacweHolders = insertBetweenPlacweHolders;
-function fileLines(path, lineSeparator = /[\n|\r]/) {
+exports.insertBetweenPlacweHoldersSync = insertBetweenPlacweHoldersSync;
+function fileLinesSync(path, lineSeparator = /[\n|\r]/) {
     var _a;
     if (!path)
         return null;
@@ -60,22 +60,22 @@ function fileLines(path, lineSeparator = /[\n|\r]/) {
         console.error(error);
     }
 }
-exports.fileLines = fileLines;
-function writeGZip(filePath, data, writeFileOptions, zLibOptions) {
+exports.fileLinesSync = fileLinesSync;
+function writeGZipSync(filePath, data, writeFileOptions, zLibOptions) {
     const buffer = data instanceof Buffer ? data : Buffer.from(data);
     const zippBuffer = (0, zlib_1.gzipSync)(buffer, zLibOptions);
     (0, fs_1.writeFileSync)(filePath, zippBuffer, writeFileOptions);
 }
-exports.writeGZip = writeGZip;
+exports.writeGZipSync = writeGZipSync;
 function readGZip(path, readFileOptions, zlibOptions) {
     const data = (0, fs_1.readFileSync)(path, readFileOptions);
     return (0, zlib_1.unzipSync)(data, zlibOptions);
 }
 exports.readGZip = readGZip;
-function serealizeObject(filePath, object) {
-    writeGZip(filePath, JSON.stringify(object));
+function serealizeObjectSync(filePath, object) {
+    writeGZipSync(filePath, JSON.stringify(object));
 }
-exports.serealizeObject = serealizeObject;
+exports.serealizeObjectSync = serealizeObjectSync;
 function deserealizeObject(filePath) {
     return JSON.parse(readGZip(filePath).toString());
 }
@@ -103,4 +103,28 @@ function appendFile(path, data, options) {
     });
 }
 exports.appendFile = appendFile;
+/**
+ * write to file, if the folder does not exist it will be recursively created
+ * @param path
+ * @param data
+ * @param options
+ * @returns
+ */
+function write(file, data, options) {
+    const dirPath = (0, path_1.dirname)(file.toString());
+    return (0, exports.exists)(dirPath).then(exist => {
+        const _opt = typeof options === 'string' ? { encoding: options } : options;
+        let promise = of();
+        if (!exist)
+            promise = (0, promises_1.mkdir)(dirPath, Object.assign(Object.assign({}, _opt), { recursive: true }));
+        return promise.then(() => (0, promises_1.writeFile)(file, data, options));
+    });
+}
+exports.write = write;
+function of(data) {
+    return new Promise((resolve, _reject) => {
+        resolve(data);
+    });
+}
+exports.of = of;
 //# sourceMappingURL=fileSys.js.map
