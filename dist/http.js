d784768f5ff0157887f05926ec1b2059d657afeb
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-04-13 12:39:22 +0200

    http add json request

--- dist/http.js
+++ dist/http.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.cookiesToObj = exports.objToCookies = exports.httpRequest = void 0;
+exports.httpHeaders = exports.cookiesToObj = exports.objToCookies = exports.httpJsonRequest = exports.httpRequest = void 0;
 const http_1 = require("http");
 const https_1 = require("https");
 const querystring_1 = require("querystring");
 const url_1 = require("url");
-function httpRequest(reqOpts, body) {
+function httpRequest(reqOpts, payload) {
     const retVAl = new Promise((resolve, reject) => {
         reqOpts = adaptRequestOpts(reqOpts);
         const reqFn = getRequestFn(reqOpts);
@@ -22,14 +22,23 @@ function httpRequest(reqOpts, body) {
         }).on('error', err => {
             reject(err);
         });
-        if (body)
-            req.write(body);
+        if (payload)
+            req.write(payload);
         req.flushHeaders();
         req.end();
     });
     return retVAl;
 }
 exports.httpRequest = httpRequest;
+function httpJsonRequest(req, data) {
+    const payload = JSON.stringify(data);
+    const reqOptions = toRequestOpts(req);
+    const headers = Object.assign({}, (reqOptions.headers || {}));
+    headers[exports.httpHeaders['Content-Type']] = 'application/json';
+    headers[exports.httpHeaders['Content-Length']] = payload.length;
+    return httpRequest(req, payload);
+}
+exports.httpJsonRequest = httpJsonRequest;
 function getProtocol(req) {
     var _a;
     let rq = req;
@@ -93,5 +102,239 @@ function adaptRequestOpts(reqOpts) {
         reqOpts.path += '?' + (0, querystring_1.stringify)(reqOpts === null || reqOpts === void 0 ? void 0 : reqOpts.searchParams);
     return reqOpts;
 }
+function toRequestOpts(reqOpts) {
+    if (!reqOpts)
+        return;
+    if (typeof reqOpts === 'object')
+        return reqOpts;
+    const url = typeof reqOpts === 'string' ? new url_1.URL(reqOpts) : reqOpts;
+    const retVal = {};
+    retVal.protocol = url.protocol;
+    retVal.port = url.port;
+    retVal.host = url.host;
+    retVal.hostname = url.hostname;
+    retVal.path = url.pathname;
+    return retVal;
+}
 //---------------------------------------------------------------------------------------------------------------------------------
+exports.httpHeaders = {
+    'Accept-Datetime': 'Accept-Datetime',
+    'Accept-Encoding': 'Accept-Encoding',
+    'Accept-Features': 'Accept-Features',
+    'Accept-Language': 'Accept-Language',
+    'Accept-Patch': 'Accept-Patch',
+    'Accept-Post': 'Accept-Post',
+    'Accept-Ranges': 'Accept-Ranges',
+    'Access-Control': 'Access-Control',
+    'Access-Control-Allow-Credentials': 'Access-Control-Allow-Credentials',
+    'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods': 'Access-Control-Allow-Methods',
+    'Access-Control-Allow-Origin': 'Access-Control-Allow-Origin',
+    'Access-Control-Expose-Headers': 'Access-Control-Expose-Headers',
+    'Access-Control-Max-Age': 'Access-Control-Max-Age',
+    'Access-Control-Request-Headers': 'Access-Control-Request-Headers',
+    'Access-Control-Request-Method': 'Access-Control-Request-Method',
+    'Age': 'Age',
+    'Allow': 'Allow',
+    'ALPN': 'ALPN',
+    'Alt-Svc': 'Alt-Svc',
+    'Alt-Used': 'Alt-Used',
+    'Alternates': 'Alternates',
+    'AMP-Cache-Transform': 'AMP-Cache-Transform',
+    'Apply-To-Redirect-Ref': 'Apply-To-Redirect-Ref',
+    'Authentication-Control': 'Authentication-Control',
+    'Authentication-Info': 'Authentication-Info',
+    'Authorization': 'Authorization',
+    'C-Ext': 'C-Ext',
+    'C-Man': 'C-Man',
+    'C-Opt': 'C-Opt',
+    'C-PEP': 'C-PEP',
+    'C-PEP-Info': 'C-PEP-Info',
+    'Cache-Control': 'Cache-Control',
+    'Cache-Status': 'Cache-Status',
+    'Cal-Managed-ID': 'Cal-Managed-ID',
+    'CalDAV-Timezones': 'CalDAV-Timezones',
+    'CDN-Cache-Control': 'CDN-Cache-Control',
+    'CDN-Loop': 'CDN-Loop',
+    'Cert-Not-After': 'Cert-Not-After',
+    'Cert-Not-Before': 'Cert-Not-Before',
+    'Clear-Site-Data': 'Clear-Site-Data',
+    'Close': 'Close',
+    'Configuration-Context': 'Configuration-Context',
+    'Connection': 'Connection',
+    'Content-Base': 'Content-Base',
+    'Content-Disposition': 'Content-Disposition',
+    'Content-Encoding': 'Content-Encoding',
+    'Content-ID': 'Content-ID',
+    'Content-Language': 'Content-Language',
+    'Content-Length': 'Content-Length',
+    'Content-Location': 'Content-Location',
+    'Content-MD5': 'Content-MD5',
+    'Content-Range': 'Content-Range',
+    'Content-Script-Type': 'Content-Script-Type',
+    'Content-Security-Policy': 'Content-Security-Policy',
+    'Content-Security-Policy-Report-Only': 'Content-Security-Policy-Report-Only',
+    'Content-Style-Type': 'Content-Style-Type',
+    'Content-Transfer-Encoding': 'Content-Transfer-Encoding',
+    'Content-Type': 'Content-Type',
+    'Content-Version': 'Content-Version',
+    'Cookie': 'Cookie',
+    'Cookie2': 'Cookie2',
+    'Cost': 'Cost',
+    'Cross-Origin-Embedder-Policy': 'Cross-Origin-Embedder-Policy',
+    'Cross-Origin-Embedder-Policy-Report-Only': 'Cross-Origin-Embedder-Policy-Report-Only',
+    'Cross-Origin-Opener-Policy': 'Cross-Origin-Opener-Policy',
+    'Cross-Origin-Opener-Policy-Report-Only': 'Cross-Origin-Opener-Policy-Report-Only',
+    'Cross-Origin-Resource-Policy': 'Cross-Origin-Resource-Policy',
+    'DASL': 'DASL',
+    'Date': 'Date',
+    'DAV': 'DAV',
+    'Default-Style': 'Default-Style',
+    'Delta-Base': 'Delta-Base',
+    'Depth': 'Depth',
+    'Derived-From': 'Derived-From',
+    'Destination': 'Destination',
+    'Differential-ID': 'Differential-ID',
+    'Digest': 'Digest',
+    'Early-Data': 'Early-Data',
+    'EDIINT-Features': 'EDIINT-Features',
+    'ETag': 'ETag',
+    'Expect': 'Expect',
+    'Expect-CT': 'Expect-CT',
+    'Expires': 'Expires',
+    'Ext': 'Ext',
+    'Forwarded': 'Forwarded',
+    'From': 'From',
+    'GetProfile': 'GetProfile',
+    'Hobareg': 'Hobareg',
+    'Host': 'Host',
+    'HTTP2-Settings': 'HTTP2-Settings',
+    'If': 'If',
+    'If-Match': 'If-Match',
+    'If-Modified-Since': 'If-Modified-Since',
+    'If-None-Match': 'If-None-Match',
+    'If-Range': 'If-Range',
+    'If-Schedule-Tag-Match': 'If-Schedule-Tag-Match',
+    'If-Unmodified-Since': 'If-Unmodified-Since',
+    'IM': 'IM',
+    'Include-Referred-Token-Binding-ID': 'Include-Referred-Token-Binding-ID',
+    'Isolation': 'Isolation',
+    'Keep-Alive': 'Keep-Alive',
+    'Label': 'Label',
+    'Last-Event-ID': 'Last-Event-ID',
+    'Last-Modified': 'Last-Modified',
+    'Link': 'Link',
+    'Location': 'Location',
+    'Lock-Token': 'Lock-Token',
+    'Man': 'Man',
+    'Max-Forwards': 'Max-Forwards',
+    'Memento-Datetime': 'Memento-Datetime',
+    'Message-ID': 'Message-ID',
+    'Meter': 'Meter',
+    'Method-Check': 'Method-Check',
+    'Method-Check-Expires': 'Method-Check-Expires',
+    'MIME-Version': 'MIME-Version',
+    'Negotiate': 'Negotiate',
+    'OData-EntityId': 'OData-EntityId',
+    'OData-Isolation': 'OData-Isolation',
+    'OData-MaxVersion': 'OData-MaxVersion',
+    'OData-Version': 'OData-Version',
+    'Opt': 'Opt',
+    'Optional-WWW-Authenticate': 'Optional-WWW-Authenticate',
+    'Ordering-Type': 'Ordering-Type',
+    'Origin': 'Origin',
+    'Origin-Agent-Cluster': 'Origin-Agent-Cluster',
+    'OSCORE': 'OSCORE',
+    'OSLC-Core-Version': 'OSLC-Core-Version',
+    'Overwrite': 'Overwrite',
+    'P3P': 'P3P',
+    'PEP': 'PEP',
+    'Pep-Info': 'Pep-Info',
+    'PICS-Label': 'PICS-Label',
+    'Ping-From': 'Ping-From',
+    'Ping-To': 'Ping-To',
+    'Position': 'Position',
+    'Pragma': 'Pragma',
+    'Prefer': 'Prefer',
+    'Preference-Applied': 'Preference-Applied',
+    'Priority': 'Priority',
+    'ProfileObject': 'ProfileObject',
+    'Protocol': 'Protocol',
+    'Protocol-Info': 'Protocol-Info',
+    'Protocol-Query': 'Protocol-Query',
+    'Protocol-Request': 'Protocol-Request',
+    'Proxy-Authenticate': 'Proxy-Authenticate',
+    'Proxy-Authentication-Info': 'Proxy-Authentication-Info',
+    'Proxy-Authorization': 'Proxy-Authorization',
+    'Proxy-Features': 'Proxy-Features',
+    'Proxy-Instruction': 'Proxy-Instruction',
+    'Proxy-Status': 'Proxy-Status',
+    'Public': 'Public',
+    'Public-Key-Pins': 'Public-Key-Pins',
+    'Public-Key-Pins-Report-Only': 'Public-Key-Pins-Report-Only',
+    'Range': 'Range',
+    'Redirect-Ref': 'Redirect-Ref',
+    'Referer': 'Referer',
+    'Referer-Root': 'Referer-Root',
+    'Refresh': 'Refresh',
+    'Repeatability-Client-ID': 'Repeatability-Client-ID',
+    'Repeatability-First-Sent': 'Repeatability-First-Sent',
+    'Repeatability-Request-ID': 'Repeatability-Request-ID',
+    'Repeatability-Result': 'Repeatability-Result',
+    'Replay-Nonce': 'Replay-Nonce',
+    'Retry-After': 'Retry-After',
+    'Safe': 'Safe',
+    'Schedule-Reply': 'Schedule-Reply',
+    'Schedule-Tag': 'Schedule-Tag',
+    'Sec-GPC': 'Sec-GPC',
+    'Sec-Token-Binding': 'Sec-Token-Binding',
+    'Sec-WebSocket-Accept': 'Sec-WebSocket-Accept',
+    'Sec-WebSocket-Extensions': 'Sec-WebSocket-Extensions',
+    'Sec-WebSocket-Key': 'Sec-WebSocket-Key',
+    'Sec-WebSocket-Protocol': 'Sec-WebSocket-Protocol',
+    'Sec-WebSocket-Version': 'Sec-WebSocket-Version',
+    'Security-Scheme': 'Security-Scheme',
+    'Server': 'Server',
+    'Server-Timing': 'Server-Timing',
+    'Set-Cookie': 'Set-Cookie',
+    'Set-Cookie2': 'Set-Cookie2',
+    'SetProfile': 'SetProfile',
+    'SLUG': 'SLUG',
+    'SoapAction': 'SoapAction',
+    'Status-URI': 'Status-URI',
+    'Strict-Transport-Security': 'Strict-Transport-Security',
+    'Sunset': 'Sunset',
+    'Surrogate-Capability': 'Surrogate-Capability',
+    'Surrogate-Control': 'Surrogate-Control',
+    'TCN': 'TCN',
+    'TE': 'TE',
+    'Timeout': 'Timeout',
+    'Timing-Allow-Origin': 'Timing-Allow-Origin',
+    'Title': 'Title',
+    'Topic': 'Topic',
+    'Traceparent': 'Traceparent',
+    'Tracestate': 'Tracestate',
+    'Trailer': 'Trailer',
+    'Transfer-Encoding': 'Transfer-Encoding',
+    'TTL': 'TTL',
+    'Upgrade': 'Upgrade',
+    'Urgency': 'Urgency',
+    'URI': 'URI',
+    'User-Agent': 'User-Agent',
+    'Variant-Vary': 'Variant-Vary',
+    'Vary': 'Vary',
+    'Version': 'Version',
+    'Via': 'Via',
+    'Want-Digest': 'Want-Digest',
+    'Warning': 'Warning',
+    'WWW-Authenticate': 'WWW-Authenticate',
+    'X-Content-Type-Options': 'X-Content-Type-Options',
+    'X-Device-Accept': 'X-Device-Accept',
+    'X-Device-Accept-Charset': 'X-Device-Accept-Charset',
+    'X-Device-Accept-Encoding': 'X-Device-Accept-Encoding',
+    'X-Device-Accept-Language': 'X-Device-Accept-Language',
+    'X-Device-User-Agent': 'X-Device-User-Agent',
+    'X-Frame-Options': 'X-Frame-Options',
+    '*': '*',
+};
 //# sourceMappingURL=http.js.map
